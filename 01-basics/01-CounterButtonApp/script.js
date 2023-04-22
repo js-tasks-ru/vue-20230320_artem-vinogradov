fd4d568eaec3467ee514c3dabf281fb77e6ec2dd
Artem Vinogradov <Artemon.waa@yandex.ru>
2023-04-22 20:00:29 +0300

    counterButtonApp - done

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,10 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  data: () => {
+    return { count: 0 };
+  },
+});
+
+createApp(App).mount('#app');
