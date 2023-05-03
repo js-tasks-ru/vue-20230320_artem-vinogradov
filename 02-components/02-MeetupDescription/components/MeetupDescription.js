b2a1af63212b75f12be570afc1f54bd16a7490bf
Artem Vinogradov <Artemon.waa@yandex.ru>
2023-05-03 22:02:55 +0300

    fix MeetupDescription

--- 02-components/02-MeetupDescription/components/MeetupDescription.js
+++ 02-components/02-MeetupDescription/components/MeetupDescription.js
@@ -8,5 +8,5 @@ export default defineComponent({
     },
   },
 
-  template: `<p class="meetup-description">{{description}}</p>`,
+  template: `<p class="meetup-description">{{ description }}</p>`,
 });
